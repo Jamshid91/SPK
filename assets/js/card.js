fc82c65e7ca08c96066f41715a99b9b874f8d4f5
Jamshid91 <iymon0217@gmail.com>
2023-07-09 10:54:22 +0500

    added

---
+++ assets/js/card.js
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const roadmapItext = new Swiper('.swiper-roadmap-text', {
+//     slidesPerView: 1,
+//     loop: true,
+//     watchOverflow: false,
+//     allowTouchMove: false,
+//       effect: 'fade',
+//       fadeEffect: {
+//         crossFade: true
+//       },
+//   });
+
+
+//   const roadmapImg = new Swiper('.swiper-roadmap-img', {
+//     slidesPerView: 1,
+//     loop: true,
+//     navigation: {
+//         nextEl: '.roadmap-next',
+//       },
+//       pagination: {
+//         el: '.roadmap-pagination',
+//         type: 'fraction',
+//         renderFraction: function(currentClass, tottalClass) {
+//             return `<span class="active-pagination' + ${currentClass} +'"></span> `+ ' / ' + `<span class="' + ${tottalClass} +'"></span> ` 
+//         }
+//       },
+//   });
+
+
+//   roadmapImg.controller.control = roadmapItext;
