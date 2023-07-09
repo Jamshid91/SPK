fdc83643c0921921195856982feb2e81f3e4ed7e
Jamshid91 <iymon0217@gmail.com>
2023-07-09 09:37:52 +0500

    added

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,7 +3,14 @@ const menu = document.querySelector('.menu'),
       menuBurger = document.querySelector('.menu-burger'),
       showMobs = document.querySelectorAll('.showMob'),
       infoBox = document.querySelectorAll('.menu-wrapper__list'),
-      body = document.querySelector('body')
+      body = document.querySelector('body'),
+      searchBox = document.querySelector('.header-search'),
+      searchInp = document.querySelector('.search__inp'),
+      searchResultsBox = document.querySelector('.search-result'),
+      searchGoods = document.querySelector('.search-goods'),
+      searchBtn = document.querySelector('.search__btn'),
+      searchClose = document.querySelector('.search__close'),
+      searchResults = document.querySelectorAll('.search-item__title');
 
 
 menuBurger.addEventListener('click', () => {
@@ -31,7 +38,63 @@ showMobs.forEach(btn => {
         })
         btn.parentElement.classList.toggle('showMenu');
     })
+});
+
+searchInp.addEventListener('focus', () => {
+    searchResultsBox.classList.remove('d-none')
+});
+
+searchInp.addEventListener('blur', () => {
+    searchResultsBox.classList.add('d-none')
+});
+
+searchBtn.addEventListener('click', () => {
+    searchBox.style.display = 'block'
+    body.classList.add('bodyStopScroll');
+});
+
+searchClose.addEventListener('click', () => {
+    searchBox.style.display = 'none'
+    body.classList.remove('bodyStopScroll');
+});
+
+window.addEventListener('click', (e) => {
+    if(e.target == searchBox) {
+        searchBox.style.display = 'none'
+        body.classList.remove('bodyStopScroll');
+    }
 })
+
+
+document.querySelector('.search__inp').oninput = function () {
+    let val = this.value.trim();
+       if(val != '') {
+        searchGoods.classList.add('d-none')
+           searchResults.forEach(elem => {
+               if(elem.textContent.search(val) == -1) {
+                   elem.innerHTML = elem.textContent; 
+                   elem.parentElement.classList.remove('hideResult')
+               } else {
+                   elem.parentElement.classList.add('hideResult');
+                   let str = elem.textContent;
+                   elem.innerHTML = insertMark(str, elem.textContent.search(val), val.length)
+               }
+           });
+    
+       }
+       else {
+        searchGoods.classList.remove('d-none')
+           searchResults.forEach(elem => {
+               elem.parentElement.classList.remove('hideResult');
+               elem.innerHTML = elem.textContent;
+           });
+       }
+       
+   }
+        
+   function insertMark(string,pos,len) {
+       return string.slice(0,pos) + "<span class='mark'>"+string.slice(pos,pos+len) +"</span>"+string.slice(pos+len)
+   };
 
 
 
